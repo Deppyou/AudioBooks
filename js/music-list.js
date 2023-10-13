baa90f60e804a7a3a4dac167dfb419aba7ce18de
Deppyou <67609974+Deppyou@users.noreply.github.com>
2023-10-13 18:45:24 +0200

    Audiobooks

---
+++ js/music-list.js
@@ -0,0 +1,93 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "The Laws of Human Nature (Part I)",
+    artist: "by Robert Greene",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "The Laws of Human Nature (Part II)",
+    artist: "by Robert Greene",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "How to become 37.78 times better at anything Atomic Habits",
+    artist: "by James Clear",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "21 Mind Traps_The Ultimate Guide to your most common Thinking errors (Part I)",
+    artist: "by Steve Burns",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "21 Mind Traps_The Ultimate Guide to your most common Thinking errors (Part II)",
+    artist: "by Steve Burns",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "THINK and GROW RICH",
+    artist: "by Napoleon Hill",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "The 7 Habits of Highly Effective People",
+    artist: "by Stephen Covey",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Rich Dad Poor Dad (Part I)",
+    artist: "by Robert Kiyosaki",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Rich Dad Poor Dad (Part II)",
+    artist: "by Robert Kiyosaki",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Limitless",
+    artist: "by Jim Kwik",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "The Psychology of Money",
+    artist: "by Morgan Housel",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "The Four Agreements",
+    artist: "by Don Miguel Ruiz",
+    img: "music-12",
+    src: "music-12"
+  },
+  
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
